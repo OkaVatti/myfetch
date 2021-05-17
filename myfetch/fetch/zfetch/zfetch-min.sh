a905deeebd9359e53492034c3dd0d412d56b3139
parker <64323718+OkaVatti@users.noreply.github.com>
2021-05-17 15:40:25 -0400

    fetch scripts
    added some WIP fetch scripts based on ufetch, pfetch, and neofetch

---
+++ myfetch/fetch/zfetch/zfetch-min.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+#
+# ufetch-arch - tiny system info for arch
+
+## INFO
+
+# user is already defined
+host="$(cat /etc/hostname)"
+os="$(hostname)"
+kernel="5.12.4 ARCH"
+packages="$(pacman -Q | wc -l)"
+shell="ZSH"
+
+## UI DETECTION
+
+parse_rcs() {
+	for f in "${@}"; do
+		wm="$(tail -n 1 "${f}" 2> /dev/null | cut -d ' ' -f 2)"
+		[ -n "${wm}" ] && echo "${wm}" && return
+	done
+}
+
+rcwm="$(parse_rcs "${HOME}/.xinitrc" "${HOME}/.xsession")"
+
+ui='unknown'
+uitype='UI'
+if [ -n "${DE}" ]; then
+	ui="${DE}"
+	uitype='DE'
+elif [ -n "${WM}" ]; then
+	ui="${WM}"
+	uitype='WM'
+elif [ -n "${XDG_CURRENT_DESKTOP}" ]; then
+	ui="${XDG_CURRENT_DESKTOP}"
+	uitype='DE'
+elif [ -n "${DESKTOP_SESSION}" ]; then
+	ui="${DESKTOP_SESSION}"
+	uitype='DE'
+elif [ -n "${rcwm}" ]; then
+	ui="${rcwm}"
+	uitype='WM'
+elif [ -n "${XDG_SESSION_TYPE}" ]; then
+	ui="${XDG_SESSION_TYPE}"
+fi
+
+ui="$(basename "${ui}")"
+
+## DEFINE COLORS
+
+# probably don't change these
+if [ -x "$(command -v tput)" ]; then
+	bold="$(tput bold)"
+	black="$(tput setaf 0)"
+	red="$(tput setaf 1)"
+	green="$(tput setaf 2)"
+	yellow="$(tput setaf 3)"
+	blue="$(tput setaf 4)"
+	magenta="$(tput setaf 5)"
+	cyan="$(tput setaf 6)"
+	white="$(tput setaf 7)"
+	reset="$(tput sgr0)"
+fi
+
+# you can change these
+lc="${reset}${bold}${cyan}"         # labels
+nc="${reset}${bold}${cyan}"         # user and hostname
+ic="${green}"                       # info
+c0="${reset}${cyan}"                # first color
+c1="${reset}${red}"
+c2="${reset}${green}"
+c3="${reset}${blue}"
+c4="${reset}${yellow}"
+
+## OUTPUT
+
+cat <<EOF
+
+${ic}^  ${nc}${USER}${ic}@${nc}${host}${reset}
+${ic}^  ${lc}----------
+${ic}^  ${lc}OPSY ~ ${ic}${os}${reset}
+${ic}^  ${lc}KRNL ~ ${ic}${kernel}${reset}
+${ic}^  ${lc}PKGS ~ ${ic}${packages}${reset}
+${ic}^  ${lc}SHLL ~ ${ic}${shell}${reset}
+${ic}^  ${lc}DEWM ~ ${ic}${ui}${reset}
+
+
+EOF
---
+++ myfetch/fetch/zfetch/zfetch.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+#
+# ufetch-arch - tiny system info for arch
+
+## INFO
+
+# user is already defined
+host="$(cat /etc/hostname)"
+os='TORII'
+kernel="5.12.2 ARCH"
+packages="$(pacman -Q | wc -l)"
+shell="ZSH"
+
+## UI DETECTION
+
+parse_rcs() {
+	for f in "${@}"; do
+		wm="$(tail -n 1 "${f}" 2> /dev/null | cut -d ' ' -f 2)"
+		[ -n "${wm}" ] && echo "${wm}" && return
+	done
+}
+
+rcwm="$(parse_rcs "${HOME}/.xinitrc" "${HOME}/.xsession")"
+
+ui='unknown'
+uitype='UI'
+if [ -n "${DE}" ]; then
+	ui="${DE}"
+	uitype='DE'
+elif [ -n "${WM}" ]; then
+	ui="${WM}"
+	uitype='WM'
+elif [ -n "${XDG_CURRENT_DESKTOP}" ]; then
+	ui="${XDG_CURRENT_DESKTOP}"
+	uitype='DE'
+elif [ -n "${DESKTOP_SESSION}" ]; then
+	ui="${DESKTOP_SESSION}"
+	uitype='DE'
+elif [ -n "${rcwm}" ]; then
+	ui="${rcwm}"
+	uitype='WM'
+elif [ -n "${XDG_SESSION_TYPE}" ]; then
+	ui="${XDG_SESSION_TYPE}"
+fi
+
+ui="$(basename "${ui}")"
+
+## DEFINE COLORS
+
+# probably don't change these
+if [ -x "$(command -v tput)" ]; then
+	bold="$(tput bold)"
+	black="$(tput setaf 0)"
+	red="$(tput setaf 1)"
+	green="$(tput setaf 2)"
+	yellow="$(tput setaf 3)"
+	blue="$(tput setaf 4)"
+	magenta="$(tput setaf 5)"
+	cyan="$(tput setaf 6)"
+	white="$(tput setaf 7)"
+	reset="$(tput sgr0)"
+fi
+
+# you can change these
+lc="${reset}${bold}${cyan}"         # labels
+nc="${reset}${bold}${cyan}"         # user and hostname
+ic="${green}"                       # info
+c0="${reset}${cyan}"                # first color
+
+## OUTPUT
+
+cat <<EOF
+
+${c0} ~-.___________.-~  ${ic}^  ${nc}${USER}${ic}@${nc}${host}${reset}
+${c0}   I  I     I  I    ${ic}^  ${lc}----------
+${c0}  ==II=======II==   ${ic}^  ${lc}OPSY ~ ${ic}${os}${reset}
+${c0}    ||       ||     ${ic}^  ${lc}KRNL ~ ${ic}${kernel}${reset}
+${c0}    ||       ||     ${ic}^  ${lc}PKGS ~ ${ic}${packages}${reset}
+${c0}    ||       ||     ${ic}^  ${lc}SHLL ~ ${ic}${shell}${reset}
+${c0}    II       II     ${ic}^  ${lc}DEWM ~ ${ic}${ui}${reset}
+
+EOF
