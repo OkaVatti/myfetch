77cedd8081334f7fb817f60c0cd3bf462eab22fe
parker <64323718+OkaVatti@users.noreply.github.com>
2021-05-17 23:44:35 -0400

    Update reifetch.sh

--- myfetch/fetch/reifetch.sh
+++ myfetch/fetch/reifetch.sh
@@ -8,7 +8,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2015-2020 parker/OkaVatti
+# Copyright (c) 2021-present parker/OkaVatti
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
